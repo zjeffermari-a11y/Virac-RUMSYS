48b4a7739334b94fa1aaca4b935089e2e2551aac
zjeffermari-a11y <zjeffermari@gmail.com>
2025-10-30 12:13:12 +0800

    Configure Docker deployment and add database seeders

---
+++ database/seeders/SectionsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Section; // Make sure you have a Section model
+
+class SectionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Section::create(['name' => 'Wet Section']);
+        Section::create(['name' => 'Dry Goods']);
+        Section::create(['name' => 'Semi-Dry']);
+    }
+}
