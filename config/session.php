a9333f3d12a726be3511a171bfed0e22cf876a34
zjeffermari-a11y <zjeffermari@gmail.com>
2025-11-01 10:43:00 +0800

    Fix PostgreSQL compatibility and update database config

--- config/session.php
+++ config/session.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('SESSION_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
@@ -73,7 +73,7 @@ return [
     |
     */
 
-    'connection' => env('SESSION_CONNECTION'),
+    'connection' => env('SESSION_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
