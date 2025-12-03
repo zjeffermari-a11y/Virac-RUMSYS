66b21be80008121533c6f3537370c16f9168c260
zjeffermari-a11y <zjeffermari@gmail.com>
2025-12-03 18:37:26 +0800

    Fix: Change session DB connection to mysql

--- config/session.php
+++ config/session.php
@@ -73,7 +73,7 @@ return [
     |
     */
 
-    'connection' => env('SESSION_CONNECTION', 'pgsql'),
+    'connection' => env('SESSION_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
